5f2b432b7bd4dcc180949b9248d3ca370e17dc8c
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-25 16:37:28 +0700

    sharing input values & adding multiple inputs

--- src/video/components/Videos/NewVideoForm.js
+++ src/video/components/Videos/NewVideoForm.js
@@ -1,24 +1,37 @@
-import { useContext } from "react";
+import { useContext, useCallback } from "react";
 import { DrawerContext } from "../../../shared/contexts/sidebar-context";
 
 import Input from "../../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../../shared/utils/validators";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../../shared/utils/validators";
 
 import "./NewVideoForm.css";
 
 const NewVideoForm = () => {
   const drawerCtx = useContext(DrawerContext);
+
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
   const videoFormClasses = drawerCtx.drawerIsOpen
     ? "new-video-form-mini video-form"
     : "new-video-form video-form";
   return (
     <form className={videoFormClasses}>
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        title="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={titleInputHandler}
       />
     </form>
   );
