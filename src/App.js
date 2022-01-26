dfdb2c46ad884aef7b39b2361b9ed7444566f3e1
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-27 00:00:47 +0700

    adding auth context for app-wide state management

--- src/App.js
+++ src/App.js
@@ -11,46 +11,58 @@ import VideoDetailPage from "./video/pages/VideoDetailPage";
 import UpdateVideo from "./video/pages/UpdateVideo";
 
 import { DrawerContext } from "./shared/contexts/sidebar-context";
+import { AuthContext } from "./shared/contexts/auth-context";
 import Auth from "./user/pages/Auth";
 
 function App() {
   const [drawerIsOpen, setDrawerIsOpen] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState();
   const openDrawer = useCallback(() => {
     setDrawerIsOpen(true);
   }, []);
   const closeDrawer = useCallback(() => {
     setDrawerIsOpen(false);
   }, []);
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
 
   return (
-    <DrawerContext.Provider
-      value={{
-        drawerIsOpen: drawerIsOpen,
-        openDrawer: openDrawer,
-        closeDrawer: closeDrawer,
-      }}
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
     >
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Videos />
-          </Route>
-          <Route path="/videos/new" exact>
-            <NewVideo />
-          </Route>
-          <Route path="/videos/:vid">
-            <UpdateVideo />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Route path="/:vid">
-            <VideoDetailPage />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    </DrawerContext.Provider>
+      <DrawerContext.Provider
+        value={{
+          drawerIsOpen: drawerIsOpen,
+          openDrawer: openDrawer,
+          closeDrawer: closeDrawer,
+        }}
+      >
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Videos />
+            </Route>
+            <Route path="/videos/new" exact>
+              <NewVideo />
+            </Route>
+            <Route path="/videos/:vid">
+              <UpdateVideo />
+            </Route>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Route path="/:vid">
+              <VideoDetailPage />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </Router>
+      </DrawerContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
