514dd20ff049b8f984a6a22a8fe636fc099aefcd
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-29 19:29:02 +0700

    update video feature

--- src/video/pages/UpdateVideo.js
+++ src/video/pages/UpdateVideo.js
@@ -1,8 +1,9 @@
-import { React, useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import { DrawerContext } from "../../shared/contexts/sidebar-context";
 import { useForm } from "../../shared/hooks/form-hook";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import {
@@ -10,192 +11,13 @@ import {
   VALIDATOR_REQUIRE,
 } from "../../shared/utils/validators";
 import "./UpdateVideo.css";
+import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
+import ErrorModal from "../../shared/components/UIElement/ErrorModal";
 
-const DUMMY_VIDEOS = [
-  {
-    id: "0gdh5QJ83fY",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "9V9I1KbJvs0",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "ubT6qNnUmQw",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "gg1bWTVGODg",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "zayVTfRIqcs",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "vPYaPNUnkqA",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "eNq-Q73ilk0",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "wp1ZltRNdJs",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "6Jtn-H2mX4g",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "BqfG0wSrVYY",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "9_O6TSyYAlE",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "zMxZovxcx-o",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "lHCMnNhxDbw",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "fS03nwOIAs8",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-  {
-    id: "3_Rxnb_E_TI",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "RDHDComps",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-];
 const UpdateVideo = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedVideo, setLoadedVideo] = useState();
   const drawerCtx = useContext(DrawerContext);
-  const [isLoading, setIsLoading] = useState(true);
   const updateVideoClasses = drawerCtx.drawerIsOpen
     ? "update-video-mini"
     : "update-video";
@@ -216,65 +38,80 @@ const UpdateVideo = () => {
     false
   );
 
-  const identifiedVideo = DUMMY_VIDEOS.find((v) => v.id === videoId);
-
   useEffect(() => {
-    if (identifiedVideo) {
-      setFormData(
-        {
-          title: {
-            value: identifiedVideo.title,
-            isValid: true,
-          },
-          description: {
-            value: identifiedVideo.description,
-            isValid: true,
-          },
-        },
-        true
-      );
-    }
-    setIsLoading(false);
-  }, [setFormData, identifiedVideo]);
+    const fetchVideo = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/videos/${videoId}`
+        );
 
+        setLoadedVideo(responseData.video);
+        setFormData(
+          {
+            title: {
+              value: responseData.video.title,
+              isValid: true,
+            },
+            description: {
+              value: responseData.video.description,
+              isValid: true,
+            },
+          },
+          true
+        );
+      } catch (err) {}
+    };
+    fetchVideo();
+  }, [sendRequest, videoId]);
+
+  if (!loadedVideo && !error) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
   const videoUpdateSubmitHandler = (event) => {
     event.preventDefault();
     console.log(formState.inputs);
   };
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <LoadingSpinner />;
   }
 
   return (
-    <div className={updateVideoClasses}>
-      <form className="update-video-form" onSubmit={videoUpdateSubmitHandler}>
-        <Input
-          id="title"
-          element="input"
-          type="text"
-          label="Title"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter a valid title."
-          onInput={inputHandler}
-          value={formState.inputs.title.value}
-          valid={formState.inputs.title.isValid}
-        />
-        <Input
-          id="description"
-          element="textarea"
-          label="Description"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid description (min 5 characters)."
-          onInput={inputHandler}
-          value={formState.inputs.description.value}
-          valid={formState.inputs.description.isValid}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          UPDATE VIDEO
-        </Button>
-      </form>
-    </div>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <div className={updateVideoClasses}>
+        <form className="update-video-form" onSubmit={videoUpdateSubmitHandler}>
+          <Input
+            id="title"
+            element="input"
+            type="text"
+            label="Title"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid title."
+            onInput={inputHandler}
+            value={formState.inputs.title.value}
+            valid={formState.inputs.title.isValid}
+          />
+          <Input
+            id="description"
+            element="textarea"
+            label="Description"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid description (min 5 characters)."
+            onInput={inputHandler}
+            value={formState.inputs.description.value}
+            valid={formState.inputs.description.isValid}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            UPDATE VIDEO
+          </Button>
+        </form>
+      </div>
+    </React.Fragment>
   );
 };
 
